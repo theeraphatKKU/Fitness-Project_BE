af08737a417e9c16e43f9b9f18786535e61d2e74
theeraphatKKU <theeraphat.se@kkumail.com>
2024-09-04 17:45:32 +0700

    first commit

---
+++ src/main/java/com/fitness/cs/fitness_project_be/FitnessProjectBeApplication.java
@@ -0,0 +1,13 @@
+package com.fitness.cs.fitness_project_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitnessProjectBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitnessProjectBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fitness/cs/fitness_project_be/FitnessProjectBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fitness.cs.fitness_project_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitnessProjectBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
